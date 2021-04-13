7afbf31c1fcd80d39865a862f5e55a5ad9dbba34
khushal <totakhailkhushal@gmail.com>
2021-04-13 13:24:18 +0200

    datastructure array

---
+++ .history/start_20210413130752.js
@@ -0,0 +1,30 @@
+// # Arrays: Introduction
+
+// ** Instructions **
+// 1. Declare a variable named`myArray` and store an array that contains 5 elements.Print the array.
+const myArray = ["shoes", "jacket", "pants", "sweater ", "t-shirt"];
+console.log(myArray);
+// console.log(myArray.length);
+
+// 2. Access the 3rd element in the array and save that to another variable named`thirdElement`.Print the`thirdElement`. 
+const thirdElement = myArray[2];
+
+console.log(thirdElement);
+
+// console.log(myArray[2]);
+
+// myArray = [2] = "thirdElement";
+// console.log(myArray);
+
+// console.log(myArray.length);
+
+// console.log([2] = "thirdElement");
+
+// 3. Reassign the fourth element of the array and give it a value of "fourth element".Print the array once more.
+myArray[3] = "fourthelement"
+console.log("myArray");
+// const target1 = myArray[3];
+// const target2 = myArray[3];
+// myArray[3] = target2;
+// myArray[3] = target1;
+// console.log(myArray[3]);
---
+++ .history/start_20210413132013.js
@@ -0,0 +1,23 @@
+// # Arrays: Introduction
+
+// ** Instructions **
+// 1. Declare a variable named`myArray` and store an array that contains 5 elements.Print the array.
+const myArray = ["shoes", "jacket", "pants", "sweater ", "t-shirt"];
+console.log(myArray);
+console.log(myArray.length);
+
+// 2. Access the 3rd element in the array and save that to another variable named`thirdElement`.Print the`thirdElement`. 
+
+const thirdElement = myArray[2];
+console.log(thirdElement);
+
+
+
+// 3. Reassign the fourth element of the array and give it a value of "fourth element".Print the array once more.
+const myArray = ["shoes", "jacket", "pants", "sweater ", "t-shirt"];
+myArray[3] = "fourth element"
+console.log(myArray);
+
+const myArray = ["shoes", "jacket", "pants", "sweater ", "t-shirt"];
+
+
